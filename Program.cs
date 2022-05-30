8afab410d059f21f34ddbb86ebd8d960a0a698c4
alice <aliiceman77@gmail.com>
2022-05-30 20:31:59 +0430

    new

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace search2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            char b;
+            int c=0;
+            Console.Write("enter a number: ");
+            a=Console.ReadLine();
+            Console.Write("enter a number that you want to search: ");
+            b=Convert.ToChar(Console.ReadLine());
+            foreach (var item in a)
+            {
+                if (item==b)
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine("adad {0}: {1} bar tekrar shode ast",b,c);
+        }
+    }
+}
